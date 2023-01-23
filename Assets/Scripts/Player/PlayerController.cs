4c22c1e452843e744e055bd9d8f4103c8b04a7cf
Alexander <47731810+gigvwvtt@users.noreply.github.com>
2023-01-23 17:23:33 +0500

    Initial commit

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController), typeof(PlayerInput))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float playerSpeed = 2f;
+    [SerializeField] private float gravityValue = -9.81f;
+    [SerializeField] private float jumpHeight = 1.0f;
+
+    private CharacterController controller;
+    private PlayerInput playerInput;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private Transform cameraTransform;
+
+    private InputAction moveAction;
+    private InputAction jumpAction;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
+        cameraTransform = Camera.main.transform;
+        moveAction = playerInput.actions["Move"];
+        jumpAction = playerInput.actions["Jump"];
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+            playerVelocity.y = 0f;
+
+        Vector2 input = moveAction.ReadValue<Vector2>();
+        Vector3 move = new Vector3(input.x, 0, input.y);
+        move = move.x * cameraTransform.right.normalized + move.z * cameraTransform.forward.normalized;
+        move.y = 0f;
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (jumpAction.triggered && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3f * gravityValue);
+        }
+        
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
